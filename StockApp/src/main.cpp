a1776dd9275de3d8e0e1ed361cc6d5a3b87f3f73
danawan0409 <122662430+danawan0409@users.noreply.github.com>
2025-04-11 17:59:14 -0400

    testing friend.cpp

--- StockApp/src/main.cpp
+++ StockApp/src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "user.h"
+#include "friend.h"
 
 int main() {
     int choice;
@@ -12,6 +13,13 @@ int main() {
         std::cout << "1. Register\n";
         std::cout << "2. Login\n";
         std::cout << "3. Exit\n";
+        std::cout << "4. Send Friend Request (test)\n";
+        std::cout << "5. View Incoming Friend Requests (test)\n";
+        std::cout << "6. View Outgoing Friend Requests (test)\n";
+        std::cout << "7. View Friends (test)\n";
+        std::cout << "8. Accept Friend Request (test)\n";
+        std::cout << "9. Reject Friend Request (test)\n";
+        std::cout << "10. Delete Friend (test)\n";
         std::cout << "Enter your choice: ";
         std::cin >> choice;
 
@@ -21,6 +29,27 @@ int main() {
                 break;
             case 2:
                 loginUser();
+                break;
+            case 4:
+                sendFriendRequest(1);
+                break;
+            case 5:
+                viewIncomingFriendRequests(1);
+                break;
+            case 6:
+                viewOutgoingFriendRequests(1); 
+                break;
+            case 7:
+                viewFriends(1); 
+                break;
+            case 8:
+                acceptFriendRequest(1); 
+                break;
+            case 9:
+                rejectFriendRequest(1); 
+                break;
+            case 10:
+                deleteFriend(1);
                 break;
             case 3:
                 running = false;
