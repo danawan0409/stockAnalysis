2f4641ae750be697dc266db3c1b863e635daa2a0
Wengxin04 <wengxin0421@outlook.com>
2025-04-12 10:52:12 -0400

    update pauseConsole to clear the screen

--- StockApp/src/global.cpp
+++ StockApp/src/global.cpp
@@ -9,4 +9,11 @@ void pauseConsole() {
     std::cout << "\nPress Enter to continue...\n";
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     std::cin.get();
+
+    // Clear the screen after user presses Enter
+#ifdef _WIN32
+    std::system("cls");
+#else
+    std::system("clear");
+#endif
 }
